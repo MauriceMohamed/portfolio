fd77d320cd469da706bf28ea9f676c0b05ac5d1a
mauriceMohamed <imohamed@live.fr>
2022-03-01 15:43:25 +0000

    almost finish header

---
+++ src/components/header/SCROLL.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const scroll = () => {
+  return (
+    <div>
+        <a href='#nav' className='scroll_down'>Scroll down</a>
+    </div>
+  )
+}
+
+export default scroll
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,6 +3,7 @@ import './header.css'
 import CTA from './cta'
 import ME from '../../assets/me.jpg'
 import HeaderSocials from './headerSocials'
+import SCROLL from './SCROLL'
 const header = () => {
   return (
     <header>
@@ -12,6 +13,8 @@ const header = () => {
         <h5 className='text-light'>junior devlopper</h5>
         <CTA />
         <HeaderSocials />
+        <SCROLL/>
+        
 
         <div className='me'>
           <img src={ME} alt='me' />
